0d4a4767b0ae71a92a9258df9a198b5898149097
WeiMing Zhang <zhangweiming1995@qq.com>
2022-07-03 22:24:00 +0800

    1. 完成第一步Config

---
+++ MyConfigServer/src/test/java/com/zwm/myconfigserver/MyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwm.myconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
